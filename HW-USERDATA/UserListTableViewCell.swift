e38cfe3294c583a451a6eb910a4917b56170b412
elio <shin.yulin0516@gmail.com>
2021-02-23 15:44:01 +0800

    Feb23

---
+++ HW-USERDATA/UserListTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  UserListTableViewCell.swift
+//  HW-USERDATA
+//
+//  Created by yulin on 2021/2/23.
+//
+
+import UIKit
+
+class UserListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
